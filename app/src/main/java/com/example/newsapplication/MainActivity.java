004c125cd172d8e5723f8335310ae8a501cfa54d
Kanak Panwar <kanak21037@iiitd.ac.in>
2022-02-21 22:36:02 +0530

    commit 3

--- app/src/main/java/com/example/newsapplication/MainActivity.java
+++ app/src/main/java/com/example/newsapplication/MainActivity.java
@@ -7,9 +7,12 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private final static String DEBUG_TAG="MainActivity";
+    Button next;
     //public int i;
 
     @Override
@@ -32,6 +35,22 @@ public class MainActivity extends AppCompatActivity {
         FragmentForNews fragmentForNews=new FragmentForNews();
         fragmentTransactionNews.replace(R.id.newsFrame,fragmentForNews);
         fragmentTransactionNews.commit();
+
+        next=findViewById(R.id.button);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Activity2.class);
+                //intent.putExtra("arrayList", symptomslist);
+                //String u = mUserInputET.getText().toString();
+                //intent.putExtra("pName", u);
+                startActivity(intent);
+                Log.i(DEBUG_TAG, "After Activity1 Intent");
+
+            }
+        });
+
+
     }
 
     @Override
